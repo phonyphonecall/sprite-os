a94cbe99b14f1133c69f3aaf09a6e09bb44a42ff
Scott Hendrickson <shendrickso2@wisc.edu>
2015-11-08 14:56:46 -0600

    bouncing ball

--- user.c
+++ user.c
@@ -1,33 +1,38 @@
 #include "user.h"
-#include "gradient.h"
+#include "pong_ball.h"
 
-uint16_t gx = 0;
-uint16_t gy = 0;
+short ball_dir_x = 1;
+short ball_dir_y = 1;
+short ball_x = 1;
+short ball_y = 1;
 
 void my_isr(void* data) {
+    if (ball_x >= 575 || ball_x <= 0) {
+        ball_dir_x *= -1;
+        sos_uart_printf("ball x dir change\n");
+    }
+    if (ball_y >= 415 || ball_y <= 0) {
+        ball_dir_y *= -1;
+        sos_uart_printf("ball y dir change\n");
+    }
+    ball_x += ball_dir_x;
+    ball_y += ball_dir_y;
+
     sos_oam_update(0x00,
-        SOS_OAM_OFFSET_X = gx;
-        SOS_OAM_OFFSET_Y = gy;
+        SOS_OAM_OFFSET_X = (uint16_t) ball_x;
+        SOS_OAM_OFFSET_Y = (uint16_t) ball_y;
     );
-    gx++;
-    gy++;
 }
 
 // Register interupts, init graphics etc...
 void sos_user_game_init() {
-    sos_vram_load_grande_chunk(0x10, ((uint8_t*) gradient));
-    sos_vram_load_grande_chunk(0x11, ((uint8_t*) gradient));
-    sos_vram_load_grande_chunk(0x12, ((uint8_t*) gradient));
-    sos_vram_load_grande_chunk(0x13, ((uint8_t*) gradient));
+    sos_vram_load_grande_chunk(0x10, ((uint8_t*) pong_ball));
 
-    sos_cram_load_palette(0x02, gradient_palette);
+    sos_cram_load_palette(0x01, pong_ball_palette);
 
-    sos_oam_set(0x00, true, 0x02, false, false, 0, 0);
-    sos_oam_set(0x01, true, 0x02, false, false, 128, 128);
-    sos_oam_set(0x02, true, 0x02, false, false, 256, 256);
-    sos_oam_set(0x03, true, 0x02, false, false, 400, 400);
-
+    sos_oam_set(0x00, true, 0x01, false, false, 0, 0);
     sos_register_vsync_cb(my_isr, 0, true);
+    sos_uart_printf("user init done\n");
 }
 
 // Called in a loop for the remainder of the game
