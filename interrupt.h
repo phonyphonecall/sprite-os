777f881e901da61be565375feb4b79eda4fbbbc1
Scott Hendrickson <shendrickso2@wisc.edu>
2015-11-08 13:52:00 -0600

    add interrupt disable

--- interrupt.h
+++ interrupt.h
@@ -5,6 +5,7 @@ typedef int sos_cb_id_t;
 typedef void (*sos_cb_t)(void*); 
 
 void sos_enable_vsync_interrupt();
+void sos_disable_vsync_interrupt();
 
 sos_cb_id_t sos_register_vsync_cb(sos_cb_t cb, void* data, bool enable);
 void sos_enable_vsync_cb(sos_cb_id_t id);
