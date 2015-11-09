8c36b545c323bdc4cc74f1931d2c311d7e7f3def
Scott Hendrickson <shendrickso2@wisc.edu>
2015-11-09 15:49:55 -0600

    simple print

--- main.c
+++ main.c
@@ -3,12 +3,12 @@
 int main(int argc, char** argv) {
     int i = 2;
     sos_uart_printf("Initializing Sprite-OS\n");
-    _sos_init_oam_queue();
+    // _sos_init_oam_queue();
     sos_uart_printf("Init done\n");
 
     sos_user_game_init();
 
-    sos_enable_vsync_interrupt();
+    // sos_enable_vsync_interrupt();
 
     while (1) {
         sos_user_game_tick();
