07ea000a6aeeac0689dae5de2ef65e674356d9c3
Scott Hendrickson <shendrickso2@wisc.edu>
2015-11-09 15:26:12 -0600

    update uart for uartlite

--- uart.c
+++ uart.c
@@ -1,7 +1,7 @@
 #include "uart.h"
 
 void sos_uart_printf(char *string) {
-    static char* std_out_map = (char*) 0x83e00000 + 0x1000;
+    static char* std_out_map = (char*) 0x84000000 + 0x0004;
 
     while (*string != '\0') {
         *std_out_map = *string;
