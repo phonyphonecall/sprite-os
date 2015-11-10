aa2c8950679a26bef1925484b16f45f20f48c296
Scott Hendrickson <shendrickso2@wisc.edu>
2015-11-10 16:29:49 -0600

    update uart to respect status regs

--- uart.c
+++ uart.c
@@ -1,10 +1,23 @@
 #include "uart.h"
 
+#define UART_STATUS_TX_FULL  (0x10000000)
+#define UART_STATUS_TX_EMPTY (0x20000000)
+
 void sos_uart_printf(char *string) {
-    static char* std_out_map = (char*) 0x84000000 + 0x0004;
+    uint32_t* status_reg = (uint32_t*) 0x84000000 + 0x0008;
+    char* tx = (char*) 0x84000000 + 0x0004;
 
     while (*string != '\0') {
-        *std_out_map = *string;
+
+        // wait until uart is ready for char
+        uint32_t status = *status_reg;
+        if (status & UART_STATUS_TX_FULL) {
+            status = *status_reg;
+            while  (!(status & UART_STATUS_TX_EMPTY)) {
+                status = *status_reg;
+            }
+        }
+        *tx = *string;
         string++;
     }
 }
--- uart.h
+++ uart.h
@@ -1,6 +1,8 @@
 #ifndef UART_H
 #define UART_H
 
+#include "sos.h"
+
 void sos_uart_printf(char* str);
 
 #endif
